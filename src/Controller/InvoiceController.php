b765d5cb2bd92ea65b770e7f61e6bb25cfa4b458
amdyDiop <amdymila@gmail.com>
2022-04-14 19:14:38 +0000

    add invoice done

---
+++ src/Controller/InvoiceController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Invoice;
+use App\Entity\InvoiceLines;
+use App\Form\InvoiceFormType;
+use App\Form\InvoicelinesFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InvoiceController extends AbstractController
+{
+    #[Route('/', name: 'app_invoice')]
+    public function index(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $tva = 18;
+        $invoice = new Invoice();
+        $invoiceLine = new Invoicelines();
+        $invoice->getInvoicelines()->add($invoiceLine);
+        $form = $this->createForm(InvoiceFormType::class, $invoice);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $lines = $invoice->getInvoicelines();
+            $last_id_record =  $em->getRepository(Invoice::class)->findOneBy([], ['id' => 'desc']);
+            $invoice->setInvoiceNumber($last_id_record ?  (int)$last_id_record->getId() + 1 : 1);
+            $em->persist($invoice);
+            foreach($lines as $singleLine ){
+                $amount_without_vat = $singleLine->getQuantity() * $singleLine->getAmount();
+                $amount_vat = ($amount_without_vat * $tva) / 100;
+                $total = $amount_vat + $amount_without_vat;
+                $singleLine->setTotalWithVat($total);
+                $singleLine->setVatAmount($amount_vat);
+                $singleLine->setInvoiceId($invoice);
+                $em->persist($singleLine);
+            }
+
+            $em->flush();
+            unset($form);
+            unset($invoice);
+            unset($line);
+            $invoice = new Invoice();
+            $line = new Invoicelines();
+            $invoice->getInvoiceLines()->add($line);
+            $form = $this->createForm(InvoiceFormType::class, $invoice);
+        }
+
+        return $this->render('invoice/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/InvoicelinesFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\InvoiceLines;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class InvoicelinesFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('description', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('quantity', NumberType::class, ['attr' => ['class' => 'form-control']])
+            ->add('amount', NumberType::class, ['attr' => ['class' => 'form-control']]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => InvoiceLines::class,
+        ]);
+    }
+}
